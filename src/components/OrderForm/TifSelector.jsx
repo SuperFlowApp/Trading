881a8d0faf5995409525efd7bc4b1ef226881838
Ehsanakr <User228.ehs@gmail.com>
2025-07-27 22:36:46 +0800

    Tif Update

--- src/components/OrderForm/TifSelector.jsx
+++ src/components/OrderForm/TifSelector.jsx
@@ -1,6 +1,3 @@
-import React, { useState } from 'react';
-import { Dropdown, Menu, Button } from 'antd';
-
 const TIF_OPTIONS = [
     { key: '1', label: 'GTC' },
     { key: '2', label: 'IOL' },
@@ -8,39 +5,24 @@ const TIF_OPTIONS = [
 ];
 
 function TifSelector({ value, onChange }) {
-    const [selected, setSelected] = useState(value || TIF_OPTIONS[0].label);
-    const [open, setOpen] = useState(false); // <-- Add open state
-    const selectedKey = TIF_OPTIONS.find(opt => opt.label === selected)?.key || TIF_OPTIONS[0].key;
-
-    const handleMenuClick = ({ key }) => {
-        const selectedLabel = TIF_OPTIONS.find(opt => opt.key === key)?.label || TIF_OPTIONS[0].label;
-        setSelected(selectedLabel);
-        if (onChange) onChange(selectedLabel);
-        setOpen(false); // Close dropdown after selection
-    };
-
-    const menu = (
-        <Menu onClick={handleMenuClick} selectedKeys={[selectedKey]}>
-            {TIF_OPTIONS.map(opt => (
-                <Menu.Item key={opt.key}>{opt.label}</Menu.Item>
-            ))}
-        </Menu>
-    );
+    const selected = value || TIF_OPTIONS[0].label;
 
     return (
         <div className="my-2">
-            <Dropdown
-                menu={menu}
-                trigger={['click']}
-                open={open}
-                onOpenChange={setOpen}
+            <select
+                className="custom-input-dropdown"
+                value={selected}
+                onChange={e => {
+                    if (onChange) onChange(e.target.value);
+                }}
+                style={{ height: 28 }}
             >
-                <Button
-                    className={`TifSelector h-[28px]${open ? ' TifSelector-open' : ''}`}
-                >
-                    {selected}
-                </Button>
-            </Dropdown>
+                {TIF_OPTIONS.map(opt => (
+                    <option key={opt.key} value={opt.label}>
+                        {opt.label}
+                    </option>
+                ))}
+            </select>
         </div>
     );
 }
