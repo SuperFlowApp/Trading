53383ebaf708416a41bc4443fe04cf049c2f21c5
Ehsanakr <User228.ehs@gmail.com>
2025-08-12 22:00:55 +0800

    fixed address

--- api/account-information.js
+++ api/account-information.js
@@ -4,11 +4,12 @@ export default async function handler(req, res) {
     return res.status(400).json({ error: "Missing Authorization header" });
   }
   try {
-    const response = await fetch('/api/account-information', {
+    const response = await fetch("https://superflow.exchange/account-information", {
+      method: "GET",
       headers: {
-        'Authorization': `Bearer ${authHeader}`,
-        'accept': 'application/json'
-      }
+        accept: "application/json",
+        Authorization: authHeader,
+      },
     });
     const data = await response.json();
     res.status(response.status).json(data);
